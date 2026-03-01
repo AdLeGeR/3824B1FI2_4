d50922b54e82d10a3bd65ffbf7141178be31c83c
AdLeGeR <dmitr1effal@yandex.ru>
2026-03-01 16:56:37 +0300

    pre release

---
+++ DmitrievA/Lab1/Polinom/List.h
@@ -0,0 +1,141 @@
+﻿#pragma once
+
+
+template <typename T>
+struct Node {
+	T data;
+	Node* next;
+	Node* prev;
+	Node(T data) {
+		this->data = data;
+		this->next = nullptr;
+		this->prev = nullptr;
+	}
+};
+
+template <typename T>
+class Iterator {
+	Node<T>* current;
+public:
+	Iterator(Node<T>* node) {
+		current = node;
+	}
+	T operator*() {
+		return current->data;
+	}
+	Iterator& operator++(int) {
+		Iterator& temp = *this;
+		current = current->next;
+		return temp;
+	}
+	Iterator& operator++() {
+		Iterator& temp = *this;
+		current = current->next;
+		return temp;
+	}
+	bool operator != (const Iterator& other) {
+		return current != other.current;
+	}
+	T* operator->() {
+		return &current->data;
+	}
+};
+
+template <typename T>
+class List {
+	friend class Iterator<T>;
+	Node<T>* first = nullptr;
+	Node<T>* last = nullptr;
+	size_t size = 0;
+	
+public:
+	typedef Iterator<T> iterator;
+	typedef Iterator<const T> const_iterator;
+	List() {}
+	void push_back(T data) {
+		Node<T>* newNode = new Node<T>(data);
+		if(size == 0) {
+			first = newNode;
+			last = newNode;
+		}
+		else {
+			last->next = newNode;
+			newNode->prev = last;
+			last = newNode;
+		}
+		size++;
+	}
+	void insert(iterator it, T data) {
+		Node<T>* newNode = new Node<T>(data);
+		if(it == iterator(first)) {
+			newNode->next = node;
+			node->prev = newNode;
+			first = newNode;
+		}
+		else if(it == nullptr) {
+			last->next = newNode;
+			newNode->prev = last;
+			last = newNode;
+		}
+		else {
+			newNode->next = node;
+			newNode->prev = node->prev;
+			node->prev->next = newNode;
+			node->prev = newNode;
+		}
+		size++;
+	}
+	void erase(iterator it) {
+		Node<T>* node = it.current;
+		if(node == first) {
+			first = node->next;
+			if(first) first->prev = nullptr;
+		}
+		else if(node == last) {
+			last = node->prev;
+			if(last) last->next = nullptr;
+		}
+		else {
+			node->prev->next = node->next;
+			node->next->prev = node->prev;
+		}
+		delete node;
+		size--;
+	}
+	iterator begin() {
+		return Iterator<T>(first);
+	}
+	iterator end() {
+		return Iterator<T>(nullptr);
+	}
+	const_iterator begin() const {
+		return const_iterator(first);
+	}
+	const_iterator end() const {
+		return const_iterator(nullptr);
+	}
+	void sort() {
+		sort_(begin(), size);
+		
+	}
+private:
+	void sort_(iterator iter, size_t len) {
+		if (len == 1)
+			return;
+		auto middle_it = iter;
+		for (size_t i = 0; i < len / 2; i++) middle_it++;
+		sort_(iter, len / 2);
+		sort_(middle_it, len - len / 2);
+		size_t m_ind = 0;
+		for (size_t i = 0; (i < len - 1) && (len / 2 + m_ind < len); i++) {
+			if (*middle_it < *iter) {
+				auto temp = *middle_it;
+				*middle_it = *iter;
+				*iter = temp;
+				middle_it++;
+				m_ind++;
+			}
+			iter++;
+		}
+	}
+};
---
+++ DmitrievA/Lab1/Polinom/Polinom.cpp
@@ -0,0 +1,64 @@
+﻿// Polinom.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Polinom.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    cout << "Choose input format of polinome:" << endl;
+    cout << "1. First number N - count of monomes.Next N pairs of numbers - monomes.First number at pair - coefficient.Second - degree(must be between 0 and 999).Example:" << endl;
+    cout << "3\n2 111\n3 123\n10 0";
+	cout << "2. Human readable format. Example: x^2y^3z^4 + 2*xy4z - 5x - 10x2y3z4 + 1\n";
+	int mode;
+	cin >> mode;
+    while (mode != 1 && mode != 2) {
+        cout << "Invalid input. Please enter 1 or 2." << endl;
+        cin >> mode;
+	}
+	Polinom polinom;
+    polinom.parseMode = (mode == 1) ? Simple : Human;
+	cout << "Enter polinome:" << endl;
+	cin >> polinom;
+	cout << "You entered: " << polinom << endl;
+    int command = -1;
+    while(command != 0) {
+        cout << "Enter command:" << endl;
+        cout << "1. Print polinome" << endl;
+		cout << "2. Add another polinome" << endl;
+        cout << "3. Multiply by another polinome" << endl;
+		cout << "4. Evaluate polinome" << endl;
+        cout << "0. Exit" << endl;
+        cin >> command;
+        if (command == 1) {
+            cout << polinom << endl;
+        }
+        else if (command == 2) {
+            Polinom other;
+            other.parseMode = polinom.parseMode;
+            cout << "Enter another polinome:" << endl;
+            cin >> other;
+            Polinom result = polinom + other;
+            cout << "Result: " << result << endl;
+        }
+        else if (command != 0) {
+            cout << "Invalid command. Please enter 0, 1 or 2." << endl;
+        }
+	}
+    std::cout << "Hello World!\n";
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
